fe6383ed6cf03cb7c9aac370184d4ef62ce177c5
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2025-05-15 13:33:13 -0400

    feat: add unit model, validation schemas, and interfaces for unit management

--- app/models/index.ts
+++ app/models/index.ts
@@ -1,6 +1,7 @@
+import Unit from '@models/unit/unit.model';
 import User from '@models/user/user.model';
 import Client from '@models/client/client.model';
 import Profile from '@models/profile/profile.model';
 import Property from '@models/property/property.model';
 
-export { Property, Profile, Client, User };
+export { Property, Profile, Client, Unit, User };

b7894ac88d59f9ed663c0a756011789f343e2707
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2026-09-19 22:29:28 -0400

    Pass requestId through service-level email and SMS queue call-sites.

--- app/services/subscription/subscription.service.ts
+++ app/services/subscription/subscription.service.ts
@@ -602,6 +602,7 @@ export class SubscriptionService {
 
         this.emailQueue.addToEmailQueue('subscriptionCanceled', {
           to: adminEmail,
+          requestId: ctx.requestId,
           emailType: MailType.SUBSCRIPTION_CANCELED,
           subject: '',
           data: {
